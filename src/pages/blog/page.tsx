572253d95018e26a6d3993edfc7eb9e8cc6bcb81
KNereSouza <professional.neresk@gmail.com>
2025-08-24 01:25:27 -0300

    feat: update Blog page

--- src/pages/blog/page.tsx
+++ src/pages/blog/page.tsx
@@ -6,17 +6,18 @@ export function Blog() {
   return (
     <div className="flex flex-col min-h-screen bg-[var(--color-secondary)]">
       <Header />
-      <main className="relative flex-grow flex flex-col items-center justify-center text-center text-[var(--color-primary)] font-sans-serif gap-8 md:gap-12 px-6">
-        <h1 className="font-bold text-xl md:text-2xl z-10">
-          Blog in Development... {":("}
+      <main className="relative p-28 flex-grow flex flex-col items-center justify-center text-center text-[var(--color-primary)] font-sans-serif gap-6 md:gap-8 px-4 sm:px-6 animate-fade-in-up">
+        <h1 className="font-bold text-2xl md:text-3xl z-10">
+          Our Blog is Coming Soon!
         </h1>
-        <p className="text-white z-10 text-base md:text-lg">
-          This section is currently under construction. Please check back soon!
+        <p className="text-white z-10 text-base md:text-lg max-w-lg">
+          We are preparing amazing content for you. Check back soon to explore
+          new ideas and tutorials!
         </p>
         <img
           src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmFjbHI4aWl6NHdteHl2bzR3Z2VmaW1zbWdlenNyb291MWZlcmtsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26hiu3mZVquuykwhy/giphy.gif"
           alt="Work in progress GIF"
-          className="z-10 w-full max-w-xs sm:max-w-sm md:max-w-md rounded-lg"
+          className="z-10 w-full max-w-xs sm:max-w-sm md:max-w-md shadow-lg shadow-black/30 ring-2 ring-[var(--color-primary)] opacity-75"
         />
         <div
           className={`absolute z-0 ${styles.blogBackground} w-full h-full`}
